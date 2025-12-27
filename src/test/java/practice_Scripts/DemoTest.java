f5d3b0ca93c3b9a48815fb7bf4128a9e3e5ab548
NAGARAJ <NAGARAJ@DESKTOP-IODHK5L>
2025-12-27 12:32:08 +0530

    Demo scripts are ready for review- Azmath

---
+++ src/test/java/practice_Scripts/DemoTest.java
@@ -0,0 +1,11 @@
+package practice_Scripts;
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("---Good morning---Anitha to Azmath----");
+	}
+
+}
+
