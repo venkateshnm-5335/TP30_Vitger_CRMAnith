d3a13fcff4b4f2680546a85fdb61370267dda6af
shaik <shaik@Azmat>
2025-12-29 04:16:55 +0530

    check

--- src/test/java/practice_Scripts/DemoTest.java
+++ src/test/java/practice_Scripts/DemoTest.java
@@ -6,6 +6,8 @@ public class DemoTest {
 		// TODO Auto-generated method stub
 System.out.println("---Good morning---Anitha to Azmath----");
 System.out.println("Azmath to Anitha");
+System.out.println("Hiiii");
+System.out.println("hello");
 	}
 
 }
